32845c967202657f858ba8892dbc872690a6b6ae
wrCisco <lbjma@tiscali.it>
2018-12-14 06:56:58 +0100

    Add day14 solution

---
+++ day14/day14.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+recipes = [3, 7]
+cur_index1 = 0
+cur_index2 = 1
+
+obj = [7, 9, 3, 0, 6, 1]
+while recipes[-6:] != obj and recipes[-7:-1] != obj:
+    recipes.extend(int(x) for x in str(sum((recipes[cur_index1], recipes[cur_index2]))))
+    cur_index1 = (cur_index1 + 1 + recipes[cur_index1]) % len(recipes)
+    cur_index2 = (cur_index2 + 1 + recipes[cur_index2]) % len(recipes)
+    # if len(recipes) in (793071, 793072):
+    #     print("Scores of the ten recipes after the first 793061:", "".join(str(x) for x in recipes[793061:793071]))
+
+if recipes[-6:] == obj:
+    print(len(recipes)-6)
+else:
+    print(len(recipes)-7)
